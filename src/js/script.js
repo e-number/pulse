d2376e4f06e4b562470363800df6baad42fdc469
Egg <Eggnom@email.com>
2022-11-05 03:30:15 +0300

    add media

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
         speed: 1200,
-        adaptiveHeight: true,
+        // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
             {
-                breakpoint: 992,
+                breakpoint: 1024,
                 settings: {
-                    dots: true,
                     arrows: false
                 }
             }
